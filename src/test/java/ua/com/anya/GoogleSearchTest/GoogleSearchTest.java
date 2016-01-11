524475a74aeed6214810e03209fe8fe5599f2ef3
anna_goncharova <anna@bandit-ltd.com>
2016-01-11 17:27:01 +0200

    small update

--- src/test/java/ua/com/anya/GoogleSearchTest/GoogleSearchTest.java
+++ src/test/java/ua/com/anya/GoogleSearchTest/GoogleSearchTest.java
@@ -4,8 +4,7 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.junit.Assert.assertEquals;
@@ -16,17 +15,15 @@ public class GoogleSearchTest {
     public void testGoogleSearch() {
         open("http://google.com/ncr");
 
-        searchField.setValue("Selenium automates browsers");
-        searchButton.click();
+        searchField.setValue("Selenium automates browsers").pressEnter();
         searchResults.shouldHaveSize(10);
-
         searchResults.get(0).shouldHave(text("Selenium automates browsers"));
 
         resultLinks.get(0).click();
 
         headerLinkOnSeleniumPage.shouldHave(exactText("Browser Automation"));
-        assertEquals("http://www.seleniumhq.org/", url() );
-        
+        assertEquals("http://www.seleniumhq.org/", url());
+
     }
 
     SelenideElement searchField = $("#sb_ifc0 input");
