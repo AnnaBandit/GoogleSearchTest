058b294e60c2f30ebc307b63223d9f365f571e45
anna_goncharova <anna@bandit-ltd.com>
2016-01-12 15:50:58 +0200

    updates after review

--- src/test/java/ua/com/anya/GoogleSearchTest/GoogleSearchTest.java
+++ src/test/java/ua/com/anya/GoogleSearchTest/GoogleSearchTest.java
@@ -3,6 +3,7 @@ package ua.com.anya.GoogleSearchTest;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -19,13 +20,14 @@ public class GoogleSearchTest {
         searchResults.shouldHaveSize(10);
         searchResults.get(0).shouldHave(text("Selenium automates browsers"));
 
+        //click on the first link in searchResults
         searchResults.get(0).find(".r>a").click();
 
         headerLinkOnSeleniumPage.shouldHave(exactText("Browser Automation"));
         assertEquals("http://www.seleniumhq.org/", url());
     }
 
-    SelenideElement searchField = $("#lst-ib");
-    ElementsCollection searchResults = $$(".srg .rc");
+    SelenideElement searchField = $(By.name("q"));
+    ElementsCollection searchResults = $$(".srg>.g");
     SelenideElement headerLinkOnSeleniumPage = $("#header h1 a");
 }
