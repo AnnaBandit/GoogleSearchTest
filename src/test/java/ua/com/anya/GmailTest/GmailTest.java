e0057864f3477ef122d3905b2a4790022c630208
anna_goncharova <anna@bandit-ltd.com>
2016-01-17 12:19:10 +0200

    small updates

--- src/test/java/ua/com/anya/GmailTest/GmailTest.java
+++ src/test/java/ua/com/anya/GmailTest/GmailTest.java
@@ -3,33 +3,33 @@ package ua.com.anya.GmailTest;
 import com.codeborne.selenide.Configuration;
 import org.junit.Test;
 import ua.com.anya.GmailTest.helpers.Config;
+import ua.com.anya.GmailTest.pages.Gmail;
+import ua.com.anya.GmailTest.pages.GmailMails;
+import ua.com.anya.GmailTest.pages.GmailMenu;
 
-import static ua.com.anya.GmailTest.helpers.Helpers.generateUniqueSubjectWithSetText;
-import static ua.com.anya.GmailTest.pages.Gmail.*;
-import static ua.com.anya.GmailTest.pages.GmailMails.*;
-import static ua.com.anya.GmailTest.pages.GmailMenu.*;
+import static ua.com.anya.GmailTest.helpers.Helpers.generateUniquePhrase;
 
 public class GmailTest {
     {
         Configuration.timeout = 15000;
     }
 
-    public String subject = generateUniqueSubjectWithSetText("Subject");
+    public String subject = generateUniquePhrase("Subject");
 
     @Test
     public void testGmail() {
-        openGmail();
-        login(Config.getUserName(), Config.getPassword());
+        Gmail.open();
+        Gmail.login(Config.getUserName(), Config.getPassword());
 
-        sendEmail(Config.getUserName(), subject);
-        refresh();
-        assertEmailExists(subject);
+        GmailMails.send(Config.getUserName(), subject);
+        GmailMails.refresh();
+        GmailMails.assertEmailExists(subject);
 
-        openSent();
-        assertEmailExists(subject);
+        GmailMenu.openSent();
+        GmailMails.assertEmailExists(subject);
 
-        openInbox();
-        searchEmailBySubject(subject);
-        assertOnlyOneEmailWithSpecifiedSubjectIsReceived(subject);
+        GmailMenu.openInbox();
+        GmailMails.searchEmailBySubject(subject);
+        GmailMails.listOfEmails.shouldHaveSize(1);
     }
 }
