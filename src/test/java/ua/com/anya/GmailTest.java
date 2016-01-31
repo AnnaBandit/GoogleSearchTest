a28ed085df35193e722124bd553dc1d19ace85c3
anna_goncharova <anna@bandit-ltd.com>
2016-01-31 15:22:05 +0200

    small changes

--- src/test/java/ua/com/anya/GmailTest.java
+++ src/test/java/ua/com/anya/GmailTest.java
@@ -2,14 +2,14 @@ package ua.com.anya;
 
 import org.junit.Test;
 import ua.com.anya.core.Configuration;
-import ua.com.anya.testData.Authentication;
 import ua.com.anya.pages.GmailMailsPage;
-import ua.com.anya.pages.GmailMenuPage;
 import ua.com.anya.pages.GmailPage;
+import ua.com.anya.testData.Authentication;
 import ua.com.anya.testconfigs.BaseTest;
 
 import static ua.com.anya.core.Asserts.assertThat;
-import static ua.com.anya.core.CustomConditions.*;
+import static ua.com.anya.core.CustomConditions.listNthElementHasText;
+import static ua.com.anya.core.CustomConditions.sizeOf;
 import static ua.com.anya.core.Helpers.generateUniquePhrase;
 import static ua.com.anya.pages.GmailMailsPage.MailFolders.INBOX;
 import static ua.com.anya.pages.GmailMailsPage.MailFolders.SENT;
@@ -23,10 +23,9 @@ public class GmailTest extends BaseTest {
 
     GmailPage gmail = new GmailPage(driver);
     GmailMailsPage gmailMails = new GmailMailsPage(driver);
-    GmailMenuPage gmailMenu = new GmailMenuPage(driver);
 
     @Test
-    public void testLoginToGmailSendAndGetLetter() {
+    public void testLogiSendMailAndSearchLetter() {
         gmail.ensureIsOpened();
         gmail.login(Authentication.userName, Authentication.password);
 
@@ -37,7 +36,6 @@ public class GmailTest extends BaseTest {
         gmailMails.searchEmailBySubject(subject, SENT);
         assertThat(listNthElementHasText(gmailMails.list, 0, subject), driver);
         gmailMails.searchEmailBySubject(subject, INBOX);
-        assertThat(listNthElementHasText(gmailMails.list, 0, subject), driver);
         assertThat(sizeOf(gmailMails.list, 1), driver);
     }
 }
